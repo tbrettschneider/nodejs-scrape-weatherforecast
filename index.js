b8fd320f1e7e6cdb277a8a1a17a0675c9dc07626
Tommy Brettschneider <tommy.brettschneider@gmail.com>
2017-06-04 18:37:53 +0200

    simplify & add usage text

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const osmosis = require('osmosis');
-location = process.argv[2];
-if (location) {
+if (location=process.argv[2]) {
     osmosis.get('www.wetter.de')
        .submit('form.wt-form', {'search': location})
        .set({
 	     'max-temperature': 'span.wt-color-temperature-max',
 	     'min-temperature': 'span.wt-color-temperature-min'
        })
-       .data(console.log)
-       .log(console.log)
-       .error(console.log)
-       .debug(console.log);
+       .data(console.log);
 } else {
-    console.log('Please pass the location as cmdline param...');
+    console.log('Usage: npm start <location>');
 }
