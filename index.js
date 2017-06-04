16428b1337970490a2c7ba92780e3ffcc87e7f90
Tommy Brettschneider <tommy.brettschneider@gmail.com>
2017-06-04 19:39:03 +0200

    format console output

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const osmosis = require('osmosis');
+const osmosis = require('osmosis'),
+      dateFormat = require('dateformat');
+
 if (location=process.argv[2]) {
     osmosis.get('www.wetter.de')
        .submit('form.wt-form', {'search': location})
@@ -8,7 +10,8 @@ if (location=process.argv[2]) {
        })
        .data(function(result) {
 	   result.location = location;	
-       	   console.log(result);
+       	   now = dateFormat(new Date(), 'dddd, mmmm dS, yyyy');
+	   console.log('%s - %s - Min %s / Max %s', now, result.location, result.temperature_min, result.temperature_max);
        });	
 } else {
     console.log('Usage: npm start <location>');
